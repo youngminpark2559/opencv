fcff6f365890fdf704e6174d41f3c8a36232a7e7
YoungMin Park <youngmtool@gmail.com>
2018-05-20 12:58:22 +0900

    Create 001.py

---
+++ sykim/001.py
@@ -0,0 +1,41 @@
+# step of digitalizing analog signal
+# 001.py
+
+# =======================================================================
+# Overview of digitalization
+# =======================================================================
+
+# Step to convert analog image into digital image:
+# sampling -> quantizing -> coding
+
+# =======================================================================
+# Sampling
+# =======================================================================
+
+# Object you're seeing is 3 dimension
+# But digital image has 2 dimension
+# This requires kind of projection to project 3d object into 2d image
+
+# =======================================================================
+# Quantization
+# =======================================================================
+
+# How many steps will you use to divide level of brightness between black and white
+# High quantization shows more natural gradation
+# img 2018-05-20 12-49-00.png
+
+# 2 level : one pixel can have white or black
+# 4 level : one pixel can have white, very white, black, very black
+# 16 level, 256 level (natural, one pixel uses 8 bit)
+# img 2018-05-20 12-51-56.png
+
+# =======================================================================
+# Coding
+# =======================================================================
+
+# Pixel actually has no shape
+# But we conviniently express pixel as rectangle
+
+# To store raw pixel data requires huge memory size
+# So you perform step of compress, which is called coding
+# img 2018-05-20 12-55-27.png
