13178631d185b15271e182ca9bd0b27945dabfd6
YoungMin Park <youngmtool@gmail.com>
2018-05-20 13:01:46 +0900

    Update 001.py

--- sykim/001.py
+++ sykim/001.py
@@ -23,11 +23,13 @@
 # How many steps will you use to divide level of brightness between black and white
 # High quantization shows more natural gradation
 # img 2018-05-20 12-49-00.png
+</xmp><img src="https://raw.githubusercontent.com/youngmtool/opencv/master/sykim/pic/2018-05-20%2012-49-00.png"><xmp>
 
 # 2 level : one pixel can have white or black
 # 4 level : one pixel can have white, very white, black, very black
 # 16 level, 256 level (natural, one pixel uses 8 bit)
 # img 2018-05-20 12-51-56.png
+</xmp><img src="https://raw.githubusercontent.com/youngmtool/opencv/master/sykim/pic/2018-05-20 12-51-56.png"><xmp>
 
 # =======================================================================
 # Coding
@@ -39,3 +41,4 @@
 # To store raw pixel data requires huge memory size
 # So you perform step of compress, which is called coding
 # img 2018-05-20 12-55-27.png
+</xmp><img src="https://raw.githubusercontent.com/youngmtool/opencv/master/sykim/pic/2018-05-20 12-55-27.png"><xmp>
