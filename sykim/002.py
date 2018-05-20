5ef4ebd87aa587046bfb41f1a06da6c4fbbca1c0
YoungMin Park <youngmtool@gmail.com>
2018-05-20 13:21:23 +0900

    Create 002.py

---
+++ sykim/002.py
@@ -0,0 +1,41 @@
+# structure and 4 types of digital image
+# 002.py
+
+# =======================================================================
+# How to express digital image
+# =======================================================================
+
+# Digital image is sort of matrix (2d array)
+
+# =======================================================================
+# How to access to pixel (2 ways)
+# =======================================================================
+
+# 1. You can use image xy coordinates (x,y)
+# Going right, value increases
+# Going down, value increases
+
+# 1. You can use matrix location by row and column (r,c)
+# Going right, value increases
+# Going down, value increases
+
+# =======================================================================
+# types of digital image
+# =======================================================================
+
+# When you use dots onto 2d array to express image, 
+# this is called bitmap image or raster image
+
+# The opposite concept with bitmap image is vector image
+
+# Bitmap font uses dot to create font
+# Vector font uses formular to create font
+
+# =======================================================================
+# approximate image
+# =======================================================================
+
+# You can use function to simply express image instead xy coordinates and matrix
+# I(x,y)
+# x,y : spatial coordinates
+# I : intensity (gray level)
